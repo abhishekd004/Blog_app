84b6f1d459e00148b719f2e02351744d1a1544a2
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-11-24 12:17:14 +0530

    Created Article Listing Page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
     def show
-        byebug #to pause the server
+        #byebug #to pause the server
         @article = Article.find(params[:id]) #params = parameter
     end
 
+    def index
+        @articles = Article.all
+    end
+    
 end
