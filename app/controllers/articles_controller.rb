91d5f71dba9bf07c40c5bfd19d45bfa3b6ccc975
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-11-25 14:46:45 +0530

    Added update feature in alpha_blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,22 +25,22 @@ class ArticlesController < ApplicationController
             render 'new'
         end        
     end
-=begin
+
     def update
         @article = Article.find(params[:id])
-        if @article.update(params:require(:article).permit(:title, :description))
+        if @article.update(params.require(:article).permit(:title, :description))
             flash[:notice] = "Article was updated successfully"
             redirect_to @article
         else
             render 'edit'
         end    
     end
-
+=begin
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
-
 =end
+
 end
