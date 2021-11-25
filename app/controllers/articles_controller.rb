9b0ec01b02e34854ee35099d0b25ba546778ed5c
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-11-25 13:29:32 +0530

    Add flash messaging and errors message display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,38 @@ class ArticlesController < ApplicationController
     end
     
     def new
+        @article = Article.new
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:articles).permit(:title, :description))
-        @article.save
-        redirect_to @article
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] ="Article was created Successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end        
+    end
+=begin
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params:require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end    
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+=end
 end
