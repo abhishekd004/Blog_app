12d2646a2a1d071be51b5a95fbff6e9d67be0b1f
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-11-30 22:44:39 +0530

    Add login page Add login and logout button

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Login successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "Login credential is wrong" #flash.now show the mssg instantly whereas flash show the mssg after one http request
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+
+end
