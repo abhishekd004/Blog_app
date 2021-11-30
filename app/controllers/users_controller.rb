2fe0f59718202915d798076cf25835592263790c
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-11-30 15:34:51 +0530

    Add profile link to article listing page Add image(gravatar) to article show page that is link to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
   def index
@@ -23,20 +23,23 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account info is updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end  
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
