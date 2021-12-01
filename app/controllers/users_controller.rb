bd78c4baed508ea5aca14a67b1ab5db49dc331d1
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-12-01 21:01:37 +0530

    Added functionality for user to delete their own account added a link in navigation bar for delete option under user dropdown list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
   before_action :require_same_user, only: [:edit, :update]
+  
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
     end  
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles are deleted"
+    redirect_to root_path
+  end
+  
   private
 
   def set_user
