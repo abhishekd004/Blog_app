07ffd3f107cd784fd43490f7f7f056b6219eaef0
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-12-02 23:32:33 +0530

    add controller test to ensure categories only created by admins Create admin user in categories controller test and sign the user in for new and create action tests Build sign_in_as helper method to test_helper.rb file, so it can be used to sign users in for tests Modify categories integration test file to create and sign in a user:

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-
+  before_action :require_admin, except: [:index, :show]
   def new
     @category = Category.new
   end
@@ -23,6 +23,13 @@ class CategoriesController < ApplicationController
   end
 
   private
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admin can perform this action"
+      redirect_to categories_path
+    end
+  end
 
   def category_params
     params.require(:category).permit(:name)
