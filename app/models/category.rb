6977e6e646acba26ef595041dabc366e353e21bb
AbhishekDusad <abhishek.dusad@unthinkable.co>
2021-12-15 21:23:32 +0530

    added acts_as_paranoid in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord 
+  acts_as_paranoid
   validates :name, presence: true, length: {minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
   has_many :article_categories
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_095018) do
+ActiveRecord::Schema.define(version: 2021_12_15_095602) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_12_09_095018) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_categories_on_deleted_at"
   end
 
   create_table "comments", force: :cascade do |t|
